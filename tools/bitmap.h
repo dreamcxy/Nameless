559945e55718bdfa2aa4b1a02932e6f6913e1858
dreamcxy <dreamcxy@gmail.com>
2020-05-05 17:14:12 +0800

    code

---
+++ tools/bitmap.h
@@ -0,0 +1,17 @@
+//
+// Created by chenxiaoyu5 on 2020/5/5.
+//
+
+#ifndef NAMELESS_BITMAP_H
+#define NAMELESS_BITMAP_H
+
+class CBitMap
+{
+public:
+    CBitMap() {}
+    ~CBitMap() {}
+
+
+};
+
+#endif //NAMELESS_BITMAP_H
